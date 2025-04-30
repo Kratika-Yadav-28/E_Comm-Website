d3a33a55128912cce677535030f09180d250ae1d
Kratika-Yadav-28 <147277416+Kratika-Yadav-28@users.noreply.github.com>
2025-04-30 13:14:15 +0530

    ECOMM-UI

---
+++ ECOMM-UI/js/api.js
@@ -0,0 +1,55 @@
+const BASE_URL="http://localhost:8080"
+
+async function loadProducts() 
+{
+    try{
+        const response = await fetch(`${BASE_URL}/products`);
+        const product = await response.json();
+
+        let trendingList = document.getElementById("trending-products");
+        let clothingList = document.getElementById("clothing-products");
+        let electronicsList = document.getElementById("electronics-products");
+
+        trendingList.innerHTML="";
+        clothingList.innerHTML="";
+        electronicsList.innerHTML="";
+
+
+        product.forEach((product) => {
+            let productCard = `
+                    <div class="col-lg-4 col-md-6">
+                        <div class="card h-100">
+                            <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}"></img>
+                            <div class="card-body d-flex flex-column">
+                                <h5 class = "card-title">${product.name}</h5>
+                                <p class="card-text">${product.description}</p>
+                                <p class="price"><strong>₹${product.price}</strong></p>
+                                <button class = "btn btn-primary mt-2" 
+                                onclick="addToCart(${product.id},'${product.name}',${product.price},'${product.imageUrl}')">
+                                Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+            `;
+
+            if(product.category==="Clothing")
+            {
+                clothingList.innerHTML+=productCard;
+            }
+            else if(product.category==="Electronics")
+            {
+                electronicsList.innerHTML+=productCard;
+            }
+            else{
+                trendingList.innerHTML+=productCard;
+            }
+
+        });
+
+    }
+    catch(error){
+        console.log("Error fetching Products:",error);
+    }
+    
+}
